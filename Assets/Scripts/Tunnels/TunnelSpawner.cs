6b847bc69f5a1db92e7af8669861eb5ed9d8581f
Stavrin <steffanleprince@gmail.com>
2022-12-03 21:14:18 +0000

    Made it so that after x amount of time a different type of tunnel can be spawned.

--- Assets/Scripts/Tunnels/TunnelSpawner.cs
+++ Assets/Scripts/Tunnels/TunnelSpawner.cs
@@ -16,36 +16,46 @@ public class TunnelSpawner : MonoBehaviour
     public bool spawned = false;
 
     public float waitTime = 4f;
+    public float expTime;
 
     void Start()
     {
-        Destroy(gameObject, waitTime);
+        expTime = Time.fixedTime;
+
+        //Destroy(gameObject, waitTime); //supposed to stop the start function happen over and over I think.
         //templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
-        Invoke("Spawn", 0.1f);
+
+        //Invoke("Spawn", 0.1f);
+
+        Spawn(openingDirection);
 
         //Invoke spawn 2 doors. pass in oD , after 3 minutes, 3 doors after 1.5mins etc.
     }
 
 
-    void Spawn()
+    void Spawn(int oD)
     {
         if (spawned == false)
         {
-            if (openingDirection == 1)
+            if ((oD == 1) && (expTime <= 0.01f))
             {
 
                 GameObject go1 = (GameObject)Resources.Load("Tunnels/Tunnel_Prefab");
+
 
                 //need to spawn a tunnel with a bottom door.
                 //rand = Random.Range(0, templates.bottomRooms.Length);
                 Instantiate(go1, transform.position, Quaternion.Euler(270, 0, 0));
             }
-            else if (openingDirection == 2)
+            else if (expTime >= 0.01f)
             {
+                GameObject go2 = (GameObject)Resources.Load("Tunnels/TTB");
+                Instantiate(go2, transform.position, Quaternion.Euler(270, 0, 0));
+
                 //rand will be between 1 and 2 now.
                 //need to spawn a tunnel with a top door.
-              //  rand = Random.Range(0, templates.topRooms.Length);
-               // Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                //  rand = Random.Range(0, templates.topRooms.Length);
+                // Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
             }
             else if (openingDirection == 3)
             {
@@ -59,7 +69,7 @@ public class TunnelSpawner : MonoBehaviour
                // rand = Random.Range(0, templates.rightRooms.Length);
                 //Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
             }
-            spawned = true;
+            //spawned = true;
         }
     }
 

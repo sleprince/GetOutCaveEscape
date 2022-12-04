32f81962594b4dfa87680bcc883bc4dc2029fcff
Stavrin <steffanleprince@gmail.com>
2022-12-04 20:16:02 +0000

    Got Petay's materials working.

--- Assets/Enlarge.cs
+++ Assets/Enlarge.cs
@@ -18,13 +18,13 @@ public class Enlarge : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        expTime = new Vector3(0.2f, 0.2f, 0.2f);
+        expTime = new Vector3(0.1f, 0.1f, 0.1f);
 
         this.transform.localScale = transform.localScale + expTime;
 
         if (Time.fixedTime >= 2)
         {
-            CameraRoot.transform.localPosition += new Vector3(0f, 0.5f, -0.5f);
+            CameraRoot.transform.localPosition += new Vector3(0f, 0.25f, -0.25f);
         }
 
 

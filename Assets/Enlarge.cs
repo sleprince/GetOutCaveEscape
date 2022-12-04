c244daca0b184660cdc89b572b98c832e27892c9
Stavrin <steffanleprince@gmail.com>
2022-12-04 17:44:19 +0000

    Made character's shrinking potion wear off over time.

--- Assets/Enlarge.cs
+++ Assets/Enlarge.cs
@@ -6,18 +6,26 @@ public class Enlarge : MonoBehaviour
 {
 
     public Vector3 expTime;
+    public GameObject CameraRoot;
 
     // Start is called before the first frame update
     void Start()
     {
-        expTime = new Vector3(Time.fixedTime, Time.fixedTime, Time.fixedTime);
+        
 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        expTime = new Vector3(0.2f, 0.2f, 0.2f);
+
         this.transform.localScale = transform.localScale + expTime;
+
+        if (Time.fixedTime >= 2)
+        {
+            CameraRoot.transform.localPosition += new Vector3(0f, 0.5f, -0.5f);
+        }
 
 
     }

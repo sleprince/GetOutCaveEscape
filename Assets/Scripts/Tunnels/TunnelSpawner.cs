64cd6c4090abf11f196bbf653ade085130d5507a
Stavrin <steffanleprince@gmail.com>
2022-12-05 15:07:23 +0000

    Trying to get randomly spawning maze working.

--- Assets/Scripts/Tunnels/TunnelSpawner.cs
+++ Assets/Scripts/Tunnels/TunnelSpawner.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class TunnelSpawner : MonoBehaviour
 {
     public int openingDirection = 1;
-    // 1 -> need bottom door.
-    // 2 -> need top door.
-    // 3 -> need left door.
-    // 4 -> need right door.
+    // 1 -> IS T > need bottom door.
+    // 2 -> IS B > need top door.
+    // 3 -> IS R > need left door.
+    // 4 -> IS L > need right door.
 
    // private RoomTemplates templates;
     private int rand;
@@ -19,6 +19,8 @@ public class TunnelSpawner : MonoBehaviour
     public float expTime;
 
     public GameObject SPoint;
+    private TunnelTemplate templates;
+    //public GameObject templates;
 
     void Start()
     {
@@ -35,8 +37,10 @@ public class TunnelSpawner : MonoBehaviour
 
         //spawned = true;
 
+        
         //Destroy(gameObject, waitTime); //supposed to stop the start function happen over and over I think.
-        //templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+
+        templates = GameObject.FindGameObjectWithTag("Tunnels").GetComponent<TunnelTemplate>();
 
         //Invoke("Spawn", 0.1f);
 
@@ -49,48 +53,51 @@ public class TunnelSpawner : MonoBehaviour
 
     void Spawn(int oD)
     {
-        if (spawned == false)
+        if (expTime <= 2.00f)
         {
-            if ((oD == 1) && (expTime <= 2.00f))
+            if (oD == 1)
             {
 
-                GameObject go1 = (GameObject)Resources.Load("Tunnels/CorridorBig");
+                //GameObject go1 = (GameObject)Resources.Load("Tunnels/CorridorBig");
 
 
                 //need to spawn a tunnel with a bottom door.
-                //rand = Random.Range(0, templates.bottomRooms.Length);
+                rand = Random.Range(0, templates.bottomTunnels.Length);
+                Instantiate(templates.bottomTunnels[rand], transform.position, templates.bottomTunnels[rand].transform.rotation);
 
                 //for the original tunnel one that needed rotating.
                 //Instantiate(go1, transform.position, Quaternion.Euler(270, 0, 0));
-                Instantiate(go1, transform.position, Quaternion.identity);
+
+
+                //Instantiate(go1, transform.position, Quaternion.identity);
 
                 //go1.transform.position = new Vector3(62, 16, 8);
 
                 //StartCoroutine(TimeHater(20.5f));
             }
-           /* else if (expTime >= 0.01f)
+           else if (openingDirection == 2)
             {
-                GameObject go2 = (GameObject)Resources.Load("Tunnels/TTB");
-                Instantiate(go2, transform.position, Quaternion.Euler(270, 0, 0));
+                //GameObject go2 = (GameObject)Resources.Load("Tunnels/TTB");
+                //Instantiate(go2, transform.position, Quaternion.Euler(270, 0, 0));
 
-                StartCoroutine(TimeHater(20.5f));
+                //StartCoroutine(TimeHater(20.5f));
 
                 //rand will be between 1 and 2 now.
                 //need to spawn a tunnel with a top door.
-                //  rand = Random.Range(0, templates.topRooms.Length);
-                // Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
-            } */
+                  rand = Random.Range(0, templates.topTunnels.Length);
+                 Instantiate(templates.topTunnels[rand], transform.position, templates.topTunnels[rand].transform.rotation);
+            }
             else if (openingDirection == 3)
             {
                 //need to spawn a tunnel with a left door.
-               // rand = Random.Range(0, templates.leftRooms.Length);
-              //  Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                rand = Random.Range(0, templates.leftTunnels.Length);
+                Instantiate(templates.leftTunnels[rand], transform.position, templates.leftTunnels[rand].transform.rotation);
             }
             else if (openingDirection == 4)
             {
                 //need to spawn a tunnel with a right door.
-               // rand = Random.Range(0, templates.rightRooms.Length);
-                //Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                rand = Random.Range(0, templates.rightTunnels.Length);
+                Instantiate(templates.rightTunnels[rand], transform.position, templates.rightTunnels[rand].transform.rotation);
             }
             //spawned = true;
             
@@ -101,7 +108,7 @@ public class TunnelSpawner : MonoBehaviour
     {
         if (other.CompareTag("SpawnPoint"))
         {
-            //if (other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            //if (other.GetComponent<Tunnelspawner>().spawned == false && spawned == false)
             //{
                // Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
                 Destroy(gameObject);
--- Assets/Scripts/Tunnels/TunnelTemplate.cs
+++ Assets/Scripts/Tunnels/TunnelTemplate.cs
@@ -10,9 +10,9 @@ public class TunnelTemplate : MonoBehaviour
     public GameObject[] leftTunnels;
     public GameObject[] rightTunnels;
 
-    public GameObject closedTunnel;
+    //public GameObject closedTunnel;
 
-    public List<GameObject> Tunnels;
+    //public List<GameObject> Tunnels;
 
     public float waitTime;
     //private bool spawnedBoss;
@@ -23,14 +23,14 @@ public class TunnelTemplate : MonoBehaviour
 
         if (waitTime <= 0) //&& spawnedBoss == false)
         {
-            for (int i = 0; i < Tunnels.Count; i++)
-            {
-                if (i == Tunnels.Count - 1)
-                {
+            //for (int i = 0; i < Tunnels.Count; i++)
+            //{
+                //if (i == Tunnels.Count - 1)
+                //{
                     //Instantiate(boss, Tunnels[i].transform.position, Quaternion.identity);
                     //spawnedBoss = true;
-                }
-            }
+                //}
+           // }
         }
         else
         {
